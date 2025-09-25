4360e0830c7960b03269c53302a1c4e7c079b90a
Nick Turner <nick+3@ncktrnr.com>
2025-09-25 16:29:17 +0800

    Rename example.gitignore to .gitignore

---
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash68d4f218287d4/1KT-KG-YFMbFJ_o2AU2hP64Dvd_iYRTRyxkYpbw2PzY.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash68d4f218287d4 extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
