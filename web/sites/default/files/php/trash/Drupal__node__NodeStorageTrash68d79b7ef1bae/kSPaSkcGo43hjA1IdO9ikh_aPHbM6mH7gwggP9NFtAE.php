d8f2f01c450b3745452efc0427c568c3928ff996
Nick Turner <nick+3@ncktrnr.com>
2025-09-27 16:15:58 +0800

    Add settings local

--- web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash68d79b7ef1bae/kSPaSkcGo43hjA1IdO9ikh_aPHbM6mH7gwggP9NFtAE.php
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash68d79b7ef1bae/kSPaSkcGo43hjA1IdO9ikh_aPHbM6mH7gwggP9NFtAE.php
@@ -5,7 +5,7 @@ use Drupal\trash\TrashStorageTrait;
 /**
  * Provides a custom storage class for trash-enabled entity types.
  */
-class Drupal__node__NodeStorageTrash68d54b15649fc extends \Drupal\node\NodeStorage {
+class Drupal__node__NodeStorageTrash68d79b7ef1bae extends \Drupal\node\NodeStorage {
 
   use TrashStorageTrait;
 
